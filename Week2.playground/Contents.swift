e46a64d3fa137244876e86de1f8e075b3e7ea333
Anibal Hernandez <anibal.hg@gmail.com>
2015-10-22 00:11:23 -0500

    First commit

---
+++ Week2.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var rango = 0...100
+
+for i in rango {
+    if i % 5 == 0 {
+        print("\(i) Bingo!!!");
+    }
+    
+    if i % 2 == 0 {
+        print("\(i) par!!!");
+    }
+    else {
+        print("\(i) impar!!!");
+    }
+    
+    if i >= 30 && i <= 40 {
+        print("\(i) Viva Swift!!!");
+    }
+}
